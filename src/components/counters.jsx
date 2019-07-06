4f207b67cb31860db13d98cd2460441a2f93f6c0
Svitlana Filatova <svitlana.filatova.gmail.com>
2019-07-06 02:17:58 -0700

    Add total counters

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-
-
   render() {
     return (
       <div>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 
-function NavBar() {
-  return (
-    <nav className="navbar navbar-light bg-light">
-    <a className="navbar-brand" href="#">Counter</a>
-    </nav>
-  );
+class NavBar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+          Navbar {" "}
+          <span className="badge badge-pill badge-secondary">
+          {this.props.totalCounters}
+          </span>
+        </a>
+      </nav>
+    );
+  }
 }
 
 export default NavBar;
